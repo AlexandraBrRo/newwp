3e9229653a1d698657fd408339c80babe302045d
AlexandraBrRo <alrobrand@gmail.com>
2023-01-16 00:32:10 +0200

    title

--- wp-content/themes/mynewanima/template-home-page.php
+++ wp-content/themes/mynewanima/template-home-page.php
@@ -7,7 +7,7 @@ get_header();
 
 <?php
 $heroSection = get_field('hero_section');
-
+$heroTitle = $heroSection['title'];
 
 
 ?>
@@ -31,8 +31,13 @@ $menuBtn = get_field('button-main', 'options');
 
 <main>
 	<div class="wrapper">
+        <div class="hero-block">
+            <div class="hero-title">
+                <h1 class="title"><?= $heroTitle ?></h1>
+            </div>
+            <a href="<?= $menuBtn['url'] ?>"target="<?= $menuBtn['target'] ?>">View all arrivals</a>
+        </div>
 
-        <a href="<?= $menuBtn['url'] ?>"target="<?= $menuBtn['target'] ?>">View all arrivals</a>
 		<?php get_footer();?>
 	</div>
 </main>
